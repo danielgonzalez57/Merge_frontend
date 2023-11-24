f5a0be5b1955f2062e5c84e24999f7122a3e971a
Christianqr <romerochristian12@hotmail.com>
2023-11-24 10:35:28 -0400

    Agg InvesProduct

--- src/views/invesProducts.vue
+++ src/views/invesProducts.vue
@@ -18,6 +18,9 @@ const router = useRouter()
 const valor = ref(false);
 const info = ref([]);
 
+const usuario = ref('')
+usuario.value = localStorage.usuario;
+
 // URL
 const id = ref('')
 id.value = route.params.key 
@@ -25,7 +28,8 @@ id.value = route.params.key
 // FUNCTION PARA LLENAR TABLE
 async function getMedicion(){
     try{
-        const response = await axios.get(`http://localhost:3001/api/v1/investProducts`);
+        //const response = await axios.get(`http://localhost:3001/api/v1/investProducts`);
+        const response = await axios.post(`http://localhost:3001/api/v1/dataInvProdFilter`, {valor: usuario.value});
         info.value =  response.data
         //console.log(info.value)
     } catch(error){
